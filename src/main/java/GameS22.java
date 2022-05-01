f809716fa3af40a3ec296b0626d652d1c539f71e
teric622 <tlasecaeric@gmail.com>
2022-05-01 17:30:17 -0400

    most up to date lab 7

--- src/main/java/GameS22.java
+++ src/main/java/GameS22.java
@@ -4,7 +4,10 @@ public class GameS22 extends Game {
         super( numRows,  numCol,  firstTeam,  secondTeam);
     }
     public boolean isAWinner(){
-        if(firstTeam.getTeamPieces().size() == 0 || secondTeam.getTeamPieces().size() ==0){
+        if(firstTeam.getTeamPieces().size() == 0 && secondTeam.getTeamPieces().size() !=0){
+            return true;
+        }
+        else if(secondTeam.getTeamPieces().size() == 0 && firstTeam.getTeamPieces().size() !=0){
             return true;
         }
         else{
@@ -18,10 +21,10 @@ public class GameS22 extends Game {
      If there is no winner, getWinner should return a null value
      */
     public Team getWinner(){
-        if(firstTeam.getTeamPieces().size() == 0){
+        if(firstTeam.getTeamPieces().size() == 0 && secondTeam.getTeamPieces().size()!=0){
             return secondTeam;
         }
-        else if (secondTeam.getTeamPieces().size() == 0){
+        else if (secondTeam.getTeamPieces().size() == 0 && firstTeam.getTeamPieces().size() != 0){
             return firstTeam;
         }
         else{
